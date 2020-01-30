69d904f7865223500481ccbec4888a43045d2c95
Brian McFeely <bmcfeely@newrelic.com>
2020-01-30 07:33:26 -0800

    Pass in reportingSourceType

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 VERSION=`cat version.go | grep VERSION | awk '{gsub(/"/, "", $4); print $4}'`
 LOCAL_IMAGE=fluent-plugin
 REMOTE_IMAGE=newrelic/newrelic-fluentbit-output
-docker build --build-arg reportingSourceVersion=${VERSION} -t ${LOCAL_IMAGE}:${VERSION} .
+docker build --build-arg reportingSourceType="docker" --build-arg reportingSourceVersion=${VERSION} -t ${LOCAL_IMAGE}:${VERSION} .
 docker tag ${LOCAL_IMAGE}:${VERSION} ${REMOTE_IMAGE}:${VERSION}
 docker push ${REMOTE_IMAGE}:${VERSION}
