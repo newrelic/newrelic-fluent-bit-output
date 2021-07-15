cc88b4c16c146b3edecba8100c66117b48ce1176
Daniel Rodríguez Gil <drodriguez@newrelic.com>
2021-07-15 20:30:37 +0200

    [LOGGING-5125] Waiting mockserver to be ready before start sending logs

--- test.sh
+++ test.sh
@@ -4,10 +4,10 @@ clean_up () {
     ARG=$?
 
     if [[ $ARG -ne 0 ]]; then
-      echo "Test failed showing docker logs"
-      echo "\n- Mockserver\n"
+      echo "Test failed, showing docker logs"
+      echo "- Mockserver"
       docker-compose -f ./test/docker-compose.yml logs mockserver
-      echo "\n- Fluent Bit\n"
+      echo "- Fluent Bit"
       docker-compose -f ./test/docker-compose.yml logs newrelic-fluent-bit-output
     fi
 
@@ -24,6 +24,11 @@ function check_logs {
   return $?
 }
 
+function check_mockserver {
+  curl -X PUT -s --fail "http://localhost:1080/mockserver/status" >> /dev/null
+  return $?
+}
+
 # Create testdata folder and log file
 mkdir ./test/testdata || true
 touch ./test/testdata/fbtest.log
@@ -37,19 +42,30 @@ fi
 echo "Starting docker compose"
 docker-compose -f ./test/docker-compose.yml up -d
 
+# Waiting mockserver to be ready
+max_retry=10
+counter=0
+while ! check_mockserver
+do
+  echo "Waiting mockserver to be ready. Trying again in 2s. Try #$counter"
+  sleep 2
+  [[ counter -eq $max_retry ]] && echo "Mockserver failed to start!" && exit 1
+  ((counter++))
+done
+
 # Sending some logs
 echo "Sending logs an waiting for arrive"
 for i in {1..5}; do
 echo "Hello!\n" >> ./test/testdata/fbtest.log
 done
 
-max_retry=3
+max_retry=10
 counter=0
 while ! check_logs
 do
-  echo "Logs not found trying again in 5s. Try #$counter"
-  sleep 5
-  [[ counter -eq $max_retry ]] && echo "Failed!" && exit 1
+  echo "Logs not found trying again in 2s. Try #$counter"
+  sleep 2
+  [[ counter -eq $max_retry ]] && echo "Logs do not reach the server!" && exit 1
   ((counter++))
 done
 echo "Success!"
