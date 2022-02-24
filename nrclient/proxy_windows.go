23989a97c876dbcf21c53c54ac3056a47d96a5df
Josep Subirats Castell <jsubirats@newrelic.com>
2022-02-24 18:52:04 +0100

    Improve misleading message that causes lots of confusion for customers

--- nrclient/proxy_windows.go
+++ nrclient/proxy_windows.go
@@ -8,8 +8,6 @@ import (
 // since Go 1.8, Go can't properly load the System root certificates on windows.
 // For more info, search Golang issues 16736 and 18609
 func systemCertPool() *x509.CertPool {
-	log.Warn("Can't load load the system root certificates. If you have set up the" +
-		" 'caBundleFile' or 'caBundleDir' configuration options, you will need to manually download the New Relic" +
-		" site certificate and store it into your CA bundle dir")
+	log.Info("Please check our documentation if you need to set up a proxy with self-signed certificates: https://github.com/newrelic/newrelic-fluent-bit-output#windows")
 	return x509.NewCertPool()
 }
